334315e07d6e1d57ceade18e229363ed2bde6f0d
Alexander Gordeev <root@localhost>
2012-03-04 21:12:09 +0400

    Set baudrate to 38400

--- eprom-burner/eprom-burner.c
+++ eprom-burner/eprom-burner.c
@@ -18,7 +18,7 @@ MCU = ATMEGA16A
 
 #define nop() asm volatile ("nop")
 
-#define USART_BAUDRATE 9600 
+#define USART_BAUDRATE 38400
 #define BAUD_PRESCALE (((F_CPU / (USART_BAUDRATE * 16UL))) - 1) 
 
 #define CRC16_LEN (2)
